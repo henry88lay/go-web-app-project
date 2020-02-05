3dd112c899a32d3ae11d186065dcf71cd0da2852
henry lay <henry88lay@gmail.com>
2020-02-05 21:18:33 +1100

    added 3 funcs towards the main.go file

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w, http.ResponseWriter, r *http.Request) {
+	w.Header().set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Let's get in touch, please send an email to <a href=\"mailto:support@photobros.com\">support@photobros.com</a>.")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	template.New("blah")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
